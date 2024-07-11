a036a345f107ae08983ff98db44e6d54b069ad09
Cole55512 <55994356+Cole55512@users.noreply.github.com>
2024-07-11 17:01:13 -0500

    Add files via upload

--- create.sql
+++ create.sql
@@ -1,7 +1,14 @@
 USE `your_database_here`;
 
+-- DEPARTMENT TABLE --
+CREATE TABLE IF NOT EXISTS `department` (
+	`dept_id` int NOT NULL DEFAULT '0',
+	`dept_name` varchar(15) NOT NULL,
+	PRIMARY KEY (`dept_id`)
+);
+
 -- EMPLOYEE TABLES --
-CREATE TABLE employee (
+CREATE TABLE IF NOT EXISTS `employee` (
     `employee_ssn` char(9) NOT NULL DEFAULT '123456789',
 	`first_name` varchar(15) NOT NULL,
 	`last_name` varchar(15) NOT NULL,
@@ -11,11 +18,12 @@ CREATE TABLE employee (
 	`username` varchar(15) NOT NULL,
 	`dept_id` int DEFAULT NULL,
     PRIMARY KEY (`employee_ssn`),
+    UNIQUE KEY `unique_username` (`username`),
 	KEY `employee_dept_id_idx` (`dept_id`),
-	CONSTRAINT `employee_dept_id` FOREIGN KEY (`dept_id`) REFERENCES `department` (`dept_id`) ON DELETE SET NULL ON UPDATE CASCADE
+	CONSTRAINT `employee_dept_id` FOREIGN KEY (`dept_id`) REFERENCES `department` (`dept_id`) ON DELETE SET DEFAULT ON UPDATE CASCADE
 );
 
-CREATE TABLE employee_credentials (
+CREATE TABLE IF NOT EXISTS `employee_credentials` (
     `employee_ssn` char(9) NOT NULL DEFAULT '123456789',
     `username` varchar(15) NOT NULL,
 	`password` varchar(15) NOT NULL,
@@ -26,18 +34,8 @@ CREATE TABLE employee_credentials (
 	CONSTRAINT `employee_username` FOREIGN KEY (`username`) REFERENCES `employee` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
--- DEPARTMENT TABLE --
-CREATE TABLE `department` (
-	`dept_id` int NOT NULL DEFAULT '0',
-	`dept_name` varchar(15) NOT NULL,
-	`manager_ssn` char(9) DEFAULT NULL,
-	PRIMARY KEY (`dept_id`),
-	KEY `manager_ssn_idx` (`manager_ssn`),
-	CONSTRAINT `manager_ssn` FOREIGN KEY (`manager_ssn`) REFERENCES `employee` (`employee_ssn`) ON DELETE SET NULL ON UPDATE CASCADE
-);
-
--- CUSTOMER TABLES --
-CREATE TABLE `customer` (
+-- CUSTOMER TABLE --
+CREATE TABLE IF NOT EXISTS `customer` (
 	`customer_id` int NOT NULL DEFAULT '0',
 	`first_name` varchar(15) NOT NULL,
 	`last_name` varchar(15) NOT NULL,
@@ -52,8 +50,21 @@ CREATE TABLE `customer` (
 	PRIMARY KEY (`customer_id`)
 );
 
--- CART TABLES --
-CREATE TABLE `cart` (
+-- PRODUCT TABLE --
+CREATE TABLE IF NOT EXISTS `product` (
+	`product_id` int NOT NULL DEFAULT '0',
+	`product_name` varchar(30) NOT NULL,
+	`sell_price` decimal(6,2) DEFAULT NULL,
+	`exp_date` char(10) DEFAULT NULL,
+	`dept_id` int NOT NULL DEFAULT '0',
+	PRIMARY KEY (`product_id`),
+	UNIQUE KEY `product_name_UNIQUE` (`product_name`),
+	KEY `product_dept_id_idx` (`dept_id`),
+	CONSTRAINT `product_dept_id` FOREIGN KEY (`dept_id`) REFERENCES `department` (`dept_id`) ON DELETE SET DEFAULT ON UPDATE CASCADE
+);
+
+-- CART TABLE --
+CREATE TABLE IF NOT EXISTS `cart` (
 	`customer_id` int NOT NULL DEFAULT '0',
 	`product_id` int NOT NULL DEFAULT '0',
 	`product_name` varchar(30) DEFAULT NULL,
@@ -65,44 +76,31 @@ CREATE TABLE `cart` (
 	CONSTRAINT `product_cart_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
--- PRODUCT TABLE --
-CREATE TABLE `product` (
-	`product_id` int NOT NULL DEFAULT '0',
-	`product_name` varchar(30) NOT NULL,
-	`sell_price` decimal(6,2) DEFAULT NULL,
-	`exp_date` char(10) DEFAULT NULL,
-	`dept_id` int NOT NULL DEFAULT '0',
-	PRIMARY KEY (`product_id`),
-	UNIQUE KEY `product_name_UNIQUE` (`product_name`),
-	KEY `product_dept_id_idx` (`dept_id`),
-	CONSTRAINT `product_dept_id` FOREIGN KEY (`dept_id`) REFERENCES `department` (`dept_id`) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
 -- SUPPLIER TABLES --
-CREATE TABLE `supplier` (
+CREATE TABLE IF NOT EXISTS `supplier` (
 	`supplier_id` int NOT NULL DEFAULT '0',
 	`supplier_name` varchar(30) NOT NULL,
 	`buy_price` decimal(10,2) DEFAULT NULL,
 	PRIMARY KEY (`supplier_id`)
 );
 
-CREATE TABLE `supplier_products` (
+CREATE TABLE IF NOT EXISTS `supplier_products` (
 	`supplier_id` int NOT NULL DEFAULT '0',
 	`supplier_product_id` int NOT NULL DEFAULT '0',
 	PRIMARY KEY (`supplier_id`,`supplier_product_id`),
 	KEY `product_supplier_id_idx` (`supplier_id`),
 	KEY `supplied_product_id_idx` (`supplier_product_id`),
-	CONSTRAINT `product_supplier_id` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`supplier_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-	CONSTRAINT `supplied_product_id` FOREIGN KEY (`supplier_product_id`) REFERENCES `product` (`product_id`) ON DELETE SET NULL ON UPDATE CASCADE
+	CONSTRAINT `product_supplier_id` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`supplier_id`) ON DELETE SET DEFAULT ON UPDATE CASCADE,
+	CONSTRAINT `supplied_product_id` FOREIGN KEY (`supplier_product_id`) REFERENCES `product` (`product_id`) ON DELETE SET DEFAULT ON UPDATE CASCADE
 );
 
-CREATE TABLE `supplied_by` (
+CREATE TABLE IF NOT EXISTS `supplied_by` (
 	`supplier_id` int NOT NULL DEFAULT '0',
 	`store_product_id` int NOT NULL DEFAULT '0',
 	`product_amount` int NOT NULL,
 	PRIMARY KEY (`supplier_id`,`store_product_id`),
 	KEY `supplied_by_id_idx` (`supplier_id`),
 	KEY `product_supplied_id_idx` (`store_product_id`),
-	CONSTRAINT `supplied_by_id` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`supplier_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-	CONSTRAINT `product_supplied_id` FOREIGN KEY (`store_product_id`) REFERENCES `product` (`product_id`) ON DELETE SET NULL ON UPDATE CASCADE
+	CONSTRAINT `supplied_by_id` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`supplier_id`) ON DELETE SET DEFAULT ON UPDATE CASCADE,
+	CONSTRAINT `product_supplied_id` FOREIGN KEY (`store_product_id`) REFERENCES `product` (`product_id`) ON DELETE SET DEFAULT ON UPDATE CASCADE
 );
