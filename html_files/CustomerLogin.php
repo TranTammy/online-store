90272aac07fe4b8e8a03757c28a150ab09810356
Cole55512 <55994356+Cole55512@users.noreply.github.com>
2024-07-26 15:37:00 -0500

    Add files via upload

---
+++ html_files/CustomerLogin.php
@@ -0,0 +1,46 @@
+<?php
+    // Customer Login Information
+    $customer_login_user = $_POST['php_customer_login_user'];
+    $customer_login_pass = $_POST['php_customer_login_pass'];
+
+    // Database Information
+    $hostname = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "database_name";
+
+    // Setup MySQL Connection
+    $connection = new mysqli($hostname, $username, $password, $dbname);
+
+    // Check Connection
+    if ($connection->connect_error) {
+        die("Connection Failed: " . $connection->connect_error);
+    }
+
+    // Prepare MySQL Statement
+    $sql_stmt = $connection->prepare("SELECT * FROM customer WHERE username = ? AND password = ?");
+    $sql_stmt->bind_param("ss", $customer_login_user, $customer_login_pass);
+
+    // Execute MySQL Statement
+    $sql_stmt->execute();
+    $result = $sql_stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        // Redirect to 'products.php'
+        header("products.php");
+        die();
+    } else {
+        // If MySQL Query Failed -> Print Error
+        if ($sql_stmt->error) {
+            echo "Error: " . $sql_stmt->error;
+        }
+        // Else --> Invalid Username or Password
+        else {
+            // TODO: Could change to make invalid username or password clearer for failed login
+            header("Location: CustomerLogin.html?error=Invalid_Username_or_Password");
+            die();
+        }
+        // Close MySQL Statements & Database Connection
+        $sql_stmt->close();
+        $connection->close();
+    }
