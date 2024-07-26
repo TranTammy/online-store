f35dd1e60f3a3d57c54a602cca94b6e82a731721
Cole55512 <55994356+Cole55512@users.noreply.github.com>
2024-07-26 15:38:43 -0500

    Add files via upload

--- html_files/CustomerLogin.php
+++ html_files/CustomerLogin.php
@@ -27,7 +27,7 @@
 
     if ($result->num_rows > 0) {
         // Redirect to 'products.php'
-        header("products.php");
+        header("Products.php");
         die();
     } else {
         // If MySQL Query Failed -> Print Error
